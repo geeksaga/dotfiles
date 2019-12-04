0fe607527cedbfe507fe9e958c95ef8ea0ca7f67
albert <albert@jennifersoft.com>
2019-12-04 11:28:15 +0900

    update link for neovim config file

--- install/link.sh
+++ install/link.sh
@@ -33,20 +33,20 @@ for config in $config_files; do
     fi
 done
 
-# create vim symlinks(use nvim)
-#echo -e "\\n\\nCreating vim symlinks"
-#echo "=============================="
-#VIMFILES=( "$HOME/.vim:$DOTFILES/config/nvim"
-#         "$HOME/.vimrc:$DOTFILES/config/nvim/init.vim" )
+# create neovim symlinks(use neovim)
+echo -e "\\n\\nCreating neovim symlinks"
+echo "=============================="
+NEOVIMFILES=( "$HOME/.config/nvim:$DOTFILES/config/nvim"
+         "$HOME/.config/nvim/init.vim:$DOTFILES/config/nvim/init.vim" )
 
-#for file in "${VIMFILES[@]}"; do
-#    KEY=${file%%:*}
-#    VALUE=${file#*:}
-#    if [ -e "${KEY}" ]; then
-#        echo "${KEY} already exists... skipping."
-#    else
-#        echo "Creating symlink for $KEY"
-#        ln -s "${VALUE}" "${KEY}"
-#    fi
-#done
+for file in "${NEOVIMFILES[@]}"; do
+    KEY=${file%%:*}
+    VALUE=${file#*:}
+    if [ -e "${KEY}" ]; then
+        echo "${KEY} already exists... skipping."
+    else
+        echo "Creating symlink for $KEY"
+        ln -s "${VALUE}" "${KEY}"
+    fi
+done
 
